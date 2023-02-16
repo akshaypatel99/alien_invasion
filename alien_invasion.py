dd2dbdbc1a630c5c2b631738d34e110fcb73279b
akshaypatel99 <akshaypatel99@gmail.com>
2023-02-16 18:50:14 +0000

    Set background color

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,9 +8,12 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources"""
         pygame.init()
-
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+        # Set the background color
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -20,8 +23,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraw the screen during each pass through the event loop
+            self.screen.fill(self.bg_color)
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == '__main__':
