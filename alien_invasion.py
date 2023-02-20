15cc28a9addcc19e6820aa28be7568b739619f91
akshaypatel99 <akshaypatel99@gmail.com>
2023-02-20 08:32:47 +0000

    Increase speed after each fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -94,6 +94,9 @@ class AlienInvasion:
         """Start a new game when the player clicks PLAY."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+
             # Reset the game statistics
             self.stats.reset_stats()
             self.game_active = True
@@ -177,6 +180,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
